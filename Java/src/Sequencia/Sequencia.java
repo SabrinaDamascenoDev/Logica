11de26b4c5ea23d77df0dabd494b6fac526c2603
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2025-02-25 21:41:06 -0300

    Questão Sequência secreta do beecrowd

---
+++ Java/src/Sequencia/Sequencia.java
@@ -0,0 +1,24 @@
+package Sequencia;
+import java.util.*;
+
+public class Sequencia {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int numerosDeValores = sc.nextInt();
+        int valores[] = new int[numerosDeValores];
+
+        for(int i = 0; i < numerosDeValores; i++){
+            valores[i] = sc.nextInt();
+        }
+        int podemSerMarcados=0;
+        for(int i = 0; i < numerosDeValores-1; i++){
+            if(valores[i] != valores[i+1]){
+                podemSerMarcados++;
+            }
+        }
+        podemSerMarcados++;
+        System.out.println(podemSerMarcados);
+
+    }
+
+}
